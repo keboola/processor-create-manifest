477b682b744de2355f4b4816448cf9f7460aea1d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-10-30 19:50:01 +0100

    fix(tests): runner

--- tests/run.php
+++ tests/run.php
@@ -10,20 +10,23 @@ foreach ($finder as $testSuite) {
     $temp = new \Keboola\Temp\Temp("processor-create-manifest");
     $temp->initRunFolder();
 
-    $copyCommand = "cp -R " . $testSuite->getPathName() . "/source " . $temp->getTmpFolder();
+    $copyCommand = "cp -R " . $testSuite->getPathName() . "/source/data/* " . $temp->getTmpFolder();
     (new \Symfony\Component\Process\Process($copyCommand))->mustRun();
 
     mkdir($temp->getTmpFolder() . "/out/tables", 0777, true);
     mkdir($temp->getTmpFolder() . "/out/files", 0777, true);
 
     $runCommand = "php /code/main.php --data=" . $temp->getTmpFolder();
-    (new \Symfony\Component\Process\Process($runCommand))->mustRun();
+    $runProcess = new \Symfony\Component\Process\Process($runCommand);
+    $runProcess->mustRun();
 
-    $diffCommand = "diff --recursive " . $testSuite->getPathName() . "/expected/data/out " . $temp->getTmpFolder() . "/out";
+    //print "\n" . $runProcess->getOutput() . "\n";
+
+    $diffCommand = "diff --ignore-all-space --recursive " . $testSuite->getPathName() . "/expected/data/out " . $temp->getTmpFolder() . "/out";
     $diffProcess = new \Symfony\Component\Process\Process($diffCommand);
     $diffProcess->run();
     if ($diffProcess->getExitCode() > 0) {
-        print $diffProcess->getOutput();
+        print "\n" . $diffProcess->getOutput() . "\n";
         exit(1);
     }
 }
