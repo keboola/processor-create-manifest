07b62f0872b088071afd4e35ee70a43300fcaa8f
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-06-06 16:23:51 +0200

    Add new DatadirTest

---
+++ tests/functional/DatadirTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Processor\CreateManifest\FunctionalTests;
+
+use Keboola\DatadirTests\DatadirTestCase;
+use Keboola\DatadirTests\DatadirTestSpecification;
+
+class DatadirTest extends DatadirTestCase
+{
+}
